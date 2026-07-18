1fed7f44f51512ac0fddf76ee3cffb8b2df2ff6a
Ajit <ajitchaurasia53@gmail.com>
2026-07-18 15:14:44 +0530

    Learning Basic

---
+++ Basic/Basic.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Basic
+{
+    public void Test()
+    {
+        int a=10;
+
+        for(int i=0; i<a; i++)
+        {
+            Console.WriteLine($"i={i}");
+        }
+    }
+}
